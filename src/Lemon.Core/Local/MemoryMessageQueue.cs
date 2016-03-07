c2418d296855af4969175db1ba54968e2e086b8d
Jeffrey Chen <support-jec@hotmail.com>
2016-03-07 20:27:14 +0800

    add local support to use lemon

---
+++ src/Lemon.Core/Local/MemoryMessageQueue.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Concurrent;
+
+namespace Lemon.Core
+{
+    public class MemoryMessageQueue : IMessageQueue
+    {
+        private ConcurrentQueue<string> _queue = new ConcurrentQueue<string>();
+
+        public void Close()
+        {
+            
+        }
+
+        public T Dequeue<T>()
+        {
+            string text;
+
+            if(_queue.TryDequeue(out text))
+            {
+               return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(text);
+            }
+
+            return default(T);
+        }
+
+        public bool Send<T>(T body)
+        {
+            var jsonFomatText = Newtonsoft.Json.JsonConvert.SerializeObject(body);
+
+            _queue.Enqueue(jsonFomatText);
+
+            return true;
+        }
+    }
+}
