6c4717f87184ef291518d654c94b67dd6439718a
Jeffrey Chen <support-jec@hotmail.com>
2016-04-14 18:27:46 +0800

    add LithiumWebApiObject

---
+++ src/eas.restapi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eas.restapi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if (args.Length < 6)
+            //{
+            //    Console.WriteLine("-name settingName -collection collectinName -handler handlerName");
+
+            //    return;
+            //}
+
+            //var parameters = new Dictionary<string, string>();
+
+            //int start = 0;
+
+            //while (start < args.Length)
+            //{
+            //    var pair = args.Skip(start).Take(2);
+
+            //    parameters.Add(pair.ElementAt(0).TrimStart('-'), pair.ElementAt(1));
+
+            //    start = start + 2;
+            //}
+        }
+    }
+}
