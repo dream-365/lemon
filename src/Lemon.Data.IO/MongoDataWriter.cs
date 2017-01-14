b9220b05f96801f35b75436ff0754622ca8c8711
Jeffrey Chen <support-jec@hotmail.com>
2017-01-14 17:29:05 +0800

    add monogdb writer/reader

---
+++ src/Lemon.Data.IO/MongoDataWriter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Lemon.Data.Core;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Lemon.Data.IO
+{
+    public class MongoDataWriter : IDataWriter<BsonDocument>
+    {
+        private IMongoCollection<BsonDocument> _collection;
+
+        public MongoDataWriter(string connectionString, string database, string collectionName)
+        {
+            var client = new MongoClient(connectionString);
+
+            _collection = client.GetDatabase(database).GetCollection<BsonDocument>(collectionName);
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public void Write(IEnumerable<BsonDocument> records)
+        {
+            _collection.InsertMany(records);
+        }
+
+        public void Write(BsonDocument record)
+        {
+            _collection.InsertOne(record);
+        }
+    }
+}
---
+++ src/Lemon.Data.IO/MongodbCollectionBase.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+
+namespace Lemon.Data.IO
+{
+    public class MongodbCollectionBase
+    {
+        protected readonly string _connectionString;
+        protected readonly string _collectionName;
+        protected readonly string _databaseName;
+        protected IMongoCollection<BsonDocument> _collection;
+
+        public MongodbCollectionBase(
+            string connectionString,
+            string databaseName,
+            string collectionName)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString) ||
+               string.IsNullOrWhiteSpace(databaseName) ||
+               string.IsNullOrWhiteSpace(collectionName))
+            {
+                throw new ArgumentNullException("connectionString, databaseName and collectionName could not be null or empty");
+            }
+            _connectionString = connectionString;
+            _collectionName = collectionName;
+            _databaseName = databaseName;
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            var client = new MongoClient(_connectionString);
+            var database = client.GetDatabase(_databaseName);
+            _collection = database.GetCollection<BsonDocument>(_collectionName);
+        }
+    }
+}
